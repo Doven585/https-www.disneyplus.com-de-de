6893168a2027231fe09754f197ba0eb8487f3e14
Doven <doven.obertinca@gmail.com>
2021-02-24 09:37:43 +0100

    login

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import Footer from "./Footer";
+import Login from "./Login";
 import NavBar from "./NavBar";
 import React from "react";
 import { Route, NavLink } from "react-router-dom";
@@ -13,13 +13,13 @@ function App() {
           <NavLink activeClassName="active" exact className="nav-link" to="/">Home</NavLink>
         </li>
         <li className="nav-item">
-          <NavLink activeClassName="active" className="nav-link" to="/footer">Login</NavLink>
+          <NavLink activeClassName="active" className="nav-link" to="/login">Login</NavLink>
         </li>
       </nav>
       <Route path="/" exact>
       </Route>
-      <Route path="/footer">
-        <Footer />
+      <Route path="/login">
+        <Login />
       </Route>
     </div>
   );
