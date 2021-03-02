fd3f9347f535ddf3b311b094a69eee61733928f6
Doven <doven.obertinca@gmail.com>
2021-03-02 16:15:25 +0100

    bacgroud black

--- src/components/App.js
+++ src/components/App.js
@@ -5,17 +5,14 @@ import React, { useState, useEffect } from "react";
 import { Route, NavLink, Redirect } from "react-router-dom";
 import Navbar from "react-bootstrap/Navbar";
 import LoginPopup from "./LoginPopup";
-import Movies from "./Movies"
+import Movies from "./Movies";
 
-export const MessengerPiggeon = React.createContext(null)
+export const MessengerPiggeon = React.createContext(null);
 
 function App() {
   const [user, setUser] = useState({
     username: "Doven",
-    favorites: [
-      "Cinderella",
-      "Ice Princess"
-    ]
+    favorites: ["Cinderella", "Ice Princess"],
   });
 
   function loginUser(userData) {
@@ -23,13 +20,12 @@ function App() {
   }
 
   function logoutUser() {
-    setUser({username: null})
+    setUser({ username: null });
   }
 
   return (
-    <MessengerPiggeon.Provider value={{user, loginUser}}>
+    <MessengerPiggeon.Provider value={{ user, loginUser }}>
       <div>
-       
         <nav>
           <li className="navbar-home">
             <NavLink className="nav-link" to="/">
@@ -37,31 +33,24 @@ function App() {
             </NavLink>
           </li>
           <li className="navbar-movies">
-            <NavLink to="/movies">
-              Movies
-            </NavLink>
+            <NavLink to="/movies">Movies</NavLink>
           </li>
           <li className="my-navbar">
             {user.username ? (
-              <button className="log-out" onClick={logoutUser}>Log out</button>
+              <button className="log-out" onClick={logoutUser}>
+                Log out
+              </button>
             ) : (
-              <NavLink to="/login">
-                Login
-              </NavLink>
+              <NavLink to="/login">Login</NavLink>
             )}
           </li>
         </nav>
         <Route path="/" exact></Route>
         <Route path="/login">
-          {user.username ?
-          
-            <Redirect to="/" />
-            : <LoginPopup />}
+          {user.username ? <Redirect to="/" /> : <LoginPopup />}
         </Route>
         <Route path="/movies">
-          
-            <Movies />
-          
+          <Movies />
         </Route>
       </div>
     </MessengerPiggeon.Provider>
