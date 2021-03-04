1e0729d141c7c6e617a5779f114dba9a5a25a74a
Doven <doven.obertinca@gmail.com>
2021-03-04 15:07:54 +0100

    position

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,11 @@
 import "./App.css";
-import Login from "./Login";
-import NavBar from "./NavBar";
 import React, { useState, useEffect } from "react";
 import { Route, NavLink, Redirect } from "react-router-dom";
-import Navbar from "react-bootstrap/Navbar";
 import LoginPopup from "./LoginPopup";
 import Movies from "./Movies";
 import * as ReactBootstrap from "react-bootstrap";
 
-import Doven from "./dovennlogo.png"
-
+import Doven from "./dovennlogo.png";
 
 export const MessengerPiggeon = React.createContext(null);
 
@@ -30,44 +26,44 @@ function App() {
   return (
     <MessengerPiggeon.Provider value={{ user, loginUser }}>
       <div className="backgruond">
-       
-
         <li className="navbar-home">
           <NavLink className="nav-link" to="/">
             <button className="btn btn-outline-secondary">Home</button>
           </NavLink>
         </li>
-          <li className="navbar-movies">
-            <NavLink to="/movies">
-              <button id="disney" className="btn btn-outline-secondary">Movies</button>
-            </NavLink>
-          </li>
-          <li className="my-navbar">
-            {user.username ? (
-              <button id="disney" 
-                className="btn btn-outline-secondary"
-                onClick={logoutUser}
-              >
-                Log out
+        <div className="logo">
+          <img src={Doven} alt="logo" width="300" height="100" />
+          <h1>This is just an exercise in React</h1>
+          <p>Some ideas are based on disneyplus!!!</p>
+        </div>
+        <div>
+          <NavLink to="/movies">
+            <button id="disney" className="btn btn-outline-secondary">
+              Movies
+            </button>
+          </NavLink>
+
+          {user.username ? (
+            <button
+              id="disney"
+              className="btn btn-outline-secondary"
+              onClick={logoutUser}
+            >
+              Log out
+            </button>
+          ) : (
+            <NavLink to="/login">
+              <button id="disney" className="btn btn-outline-secondary">
+                Login
               </button>
-            ) : (
-              <NavLink to="/login">
-                <button id="disney" className="btn btn-outline-secondary">Login</button>
-              </NavLink>
-            )}
-          </li>
+            </NavLink>
+          )}
+        </div>
         <Route path="/" exact></Route>
         <Route path="/login">
           {user.username ? <Redirect to="/" /> : <LoginPopup />}
         </Route>
-        <div className="logo">
-         <img src={Doven} alt="logo" width="300" height="100"/>
-          <h1>This is just an exercise in React</h1>
-          <p>Some ideas are based on disneyplus!!!</p>
-          
-        
-          
-        </div>
+
         <Route path="/movies">
           <Movies />
         </Route>
